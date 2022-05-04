cf7330ee18df4c4d555940d4cb972c98f09b6c8a
Rosaldo <rosaldo@gmail.com>
2022-05-04 00:12:37 -0300

    v1.3.0 alpha - crud basico: criando o template index.html e o form.html

--- cars/views.py
+++ cars/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, "index.html")
+
+
+def form(request):
+    return render(request, "form.html")
--- crud/urls.py
+++ crud/urls.py
@@ -13,11 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from cars.views import home
+from cars.views import form, home
 from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", home),
+    path("form/", form),
 ]
